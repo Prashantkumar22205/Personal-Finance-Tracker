3ae94d0ffa7ddff0127bbc3b5d64c453a066273e
Prashant Kumar <prashantkumar22205@gmail.com>
2026-05-29 16:18:36 +0530

    Fix port configuration for deployment

--- backend-project/server.js
+++ backend-project/server.js
@@ -4,6 +4,8 @@ const ConnetDB = require("./src/config/db")
 const app = require("./src/app")
  ConnetDB();
 
-app.listen(3000,()=>{
-    console.log("server is running !!")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
